1fe1b1f930f75d3ae9b281062364ab31fd77dc12
Filippo Schierato <cioffosplat@gmail.com>
2024-04-13 01:33:28 +0200

    Fixed paintbrush

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Saving function for the themes
 window.onload = function() {
     const savedTheme = sessionStorage.getItem('theme');
     if (savedTheme) {
@@ -7,10 +8,20 @@ window.onload = function() {
     }
 }
 
+//Paintbrush Menu
+function togglePaintbrushMenu() {
+    const paintbrushMenu = document.getElementById('paintbrushMenu');
+    paintbrushMenu.classList.toggle('show');
+}
+document.getElementById('paintbrushButton').addEventListener('click', togglePaintbrushMenu);
+
+
+//Setting of the Themes
 function setTheme(theme) {
     sessionStorage.setItem('theme', theme);
     document.getElementById('header').style.backgroundColor = 'var(--' + theme + '-secondary-color)';
     document.getElementById('body').style.backgroundColor = 'var(--' + theme + '-background-color)';
+    document.getElementById('paintbrushMenu').style.backgroundColor = 'var(--' + theme + '-accent-color)';
     document.getElementById('logo').src = 'resources/images/logos/logo' + theme.charAt(0).toUpperCase() + theme.slice(1) + '.jpg';
     document.getElementById('userIcon').src = 'resources/images/users/defaultUser' + theme.charAt(0).toUpperCase() + theme.slice(1) + '.jpg';
     document.getElementById('paintbrushButton').src = `resources/images/paintbrush/paintbrush${theme.charAt(0).toUpperCase() + theme.slice(1)}.png`;
